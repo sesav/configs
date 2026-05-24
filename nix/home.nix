e7a5e197d8fb95c54ed2ef22626797be7fe82cf7
Sergey Silaev <hello@8am.dev>
2026-05-24 11:38:26 +0200

    Update home.nix, flake.lock

--- nix/home.nix
+++ nix/home.nix
@@ -2,10 +2,6 @@
 {
   home.stateVersion = "25.11";
 
-  # imports = [
-  #   ./neovim.nix
-  # ];
-
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
@@ -13,12 +9,85 @@
 
   home.packages = with pkgs; [
     # Lang & runtimes
+    go
     lua
     luarocks
     nodejs_24
     ranger
     uv
-    zig
+
+    # Dev
+    bat
+    btop
+    ctags
+    fd
+    fzf
+    gh
+    git
+    jq
+    just
+    mc
+    pgcli
+    restic
+    ripgrep
+    ruff
+    stylua
+    tig
+    tldr
+    tmux
+    tokei
+    tree
+    neovim
+    tree-sitter
+    watchexec
+    zoxide
+
+    # GNU utilities
+    coreutils
+    diffutils
+    findutils
+    gawk
+    gcc
+    gettext
+    gnugrep
+    gnumake
+    gnused
+    gnutar
+    gzip
+    patch
+    unzip
+    which
+
+    # Network tools
+    curl
+    iperf
+    nmap
+    syncthing
+    trippy
+    wget
+    wrk
+
+    # DevOps & Cloud
+    ansible
+    kubectl
+    kubectx
+
+    # Fonts
+    nerd-fonts.fira-code
+    nerd-fonts.droid-sans-mono
+    nerd-fonts.noto
+    nerd-fonts.hack
+    nerd-fonts.ubuntu
+    nerd-fonts.jetbrains-mono
+
+    # Other
+    home-manager
+    zola
+  ] ++ pkgs.lib.optionals (!pkgs.stdenv.isLinux) [
+    # Shells
+    bash
+    fish
+
     (python314.withPackages (ps: with ps; [
       ipykernel
       ipython
@@ -30,56 +99,6 @@
       msgspec
       pydantic
     ]))
-
-    # DevOps & Cloud
-    ansible
-    k9s
-    # Temporary workaround for nixpkgs avro-c++ build failure on darwin.
-    (kcat.overrideAttrs (old: {
-      buildInputs = builtins.filter
-        (pkg: !(builtins.elem pkg [ avro-c libserdes ]))
-        old.buildInputs;
-    }))
-    kubectl
-    kubectx
-    kubernetes-helm
-    minikube
-
-    # Databases
-    # postgresql_18
-    # redis
-
-    # Other
-    home-manager
-    zola
-  ] ++ pkgs.lib.optionals (!pkgs.stdenv.isLinux) [
-    autoconf
-    bat
-    btop
-    cmake
-    fd
-    flex
-    go
-    jq
-    less
-    m4
-    mc
-    rclone
-    restic
-    ripgrep
-    screen
-    starship
-    swig
-    tldr
-    tree
-    tree-sitter
-    unzip
-    watch
-    zip
-
-    # Shells
-    bash
-    fish
 
     # GNU utilities
     coreutils
@@ -108,33 +127,6 @@
         (dep: (dep.pname or "") != "secretstorage")
         old.dependencies;
     }))
-
-    # Network tools
-    curl
-    iperf
-    nmap
-    syncthing
-    trippy
-    wget
-    wrk
-
-    # Dev
-    ctags
-    fzf
-    gh
-    git
-    just
-    pgcli
-    ruff
-    stylua
-    tig
-    tmux
-    tokei
-    watchexec
-    zoxide
-
-    # Shell enhancements
-    zsh-syntax-highlighting
 
     # Security & crypto
     age
